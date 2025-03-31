37f0204d68d49fb48aa32f09dc6d492bd9afc838
Agustin Galante <agustin.galante.19@outlook.es>
2025-03-31 13:55:04 -0300

    new colors definition

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -2,6 +2,7 @@ export const Colors = {
   background: '#000',
   text: '#fff',
   primary: '#2688EB',
-  secondary: '#1C1E21',
-  off: '#868686',
+  secondary: '#141414',
+  lightGray: '#9d9da3',
+  gray: '#2f2f31',
 };
